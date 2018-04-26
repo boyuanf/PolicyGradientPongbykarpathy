dfe56217bf13cc2ca9baf80e006df8e271db1cc5
boyuanf <fengboyuannj@gmail.com>
2018-04-26 17:50:46 -0400

    change original code to readable style and gradient descent

--- pg_pong_gd.py
+++ pg_pong_gd.py
@@ -1,4 +1,6 @@
 """ Trains an agent with (stochastic) Policy Gradients on Pong. Uses OpenAI Gym. """
+""" Change the gradient ascent of the orignial code to gradient decent, also make the calculation follow the formula"""
+
 import numpy as np
 import pickle
 import gym
@@ -10,7 +12,7 @@ learning_rate = 1e-4
 gamma = 0.99  # discount factor for reward
 decay_rate = 0.99  # decay factor for RMSProp leaky sum of grad^2
 resume = False  # resume from previous checkpoint?
-render = True
+render = False
 
 # model initialization
 D = 80 * 80  # input dimensionality: 80x80 grid
@@ -19,8 +21,9 @@ if resume:
 else:
     model = {}
     # np.random.randn: Return a sample (or samples) from the “standard normal” distribution.
-    model['W1'] = np.random.randn(H, D) / np.sqrt(D)  # "Xavier" initialization: setting the variance of W[l] to sqrt(1/n[l-1]) and mean to 0
-    model['W2'] = np.random.randn(H) / np.sqrt(H)
+    model['W1'] = np.random.randn(H, D) / np.sqrt(
+        D)  # "Xavier" initialization: setting the variance of W[l] to sqrt(1/n[l-1]) and mean to 0
+    model['W2'] = np.random.randn(1, H) / np.sqrt(H)
 
 grad_buffer = {k: np.zeros_like(v) for k, v in model.items()}  # update buffers that add up gradients over a batch
 rmsprop_cache = {k: np.zeros_like(v) for k, v in model.items()}  # rmsprop memory
@@ -46,46 +49,40 @@ def discount_rewards(r):
     discounted_r = np.zeros_like(r)
     running_add = 0
     for t in reversed(range(0, r.size)):
-        if r[t] != 0: running_add = 0  # reset the sum, since this was a game boundary (pong specific!)
+        if r[t] != 0:
+            running_add = 0  # reset the sum, since this was a game boundary (pong specific!)
         running_add = running_add * gamma + r[t]
         discounted_r[t] = running_add
     return discounted_r
 
 
 def policy_forward(x):
-    h = np.dot(model['W1'], x)
-    h[h < 0] = 0  # ReLU nonlinearity
-    logp = np.dot(model['W2'], h)
-    p = sigmoid(logp)
-    '''   # debug
-    print("x shape: ", x.shape)  # x shape:  (6400,)
-    print("h shape: ", h.shape)  # h shape:  (200,)
-    print("logp shape: ", logp.shape) # logp shape:  ()
-    '''
-    return p, h  # return probability of taking action 2, and hidden state
+    W1 = model['W1']
+    W2 = model['W2']
+    Z1 = np.dot(model['W1'], x)  # x shape:  (6400,1)
+    A1 = Z1
+    A1[A1 < 0] = 0  # ReLU nonlinearity, A1 shape:  (200,)
+    Z2 = np.dot(model['W2'], A1)  # Z2 shape (1)
+    A2 = sigmoid(Z2)
+    return A1, A2, Z1  # return probability of taking action 2, and hidden state
 
 
-def policy_backward(eph, epdlogp):  # parameter (A1, dZ2)
-    """ backward pass. (eph is array of intermediate hidden states) """
-    dW2 = np.dot(eph.T, epdlogp).ravel() # dW2 = A1.T multi dZ2
-    '''
-    print("eph shape: ", eph.shape)  # eph shape:  (1227, 200)
-    print("epdlogp shape: ", epdlogp.shape)  # epdlogp shape:  (1227, 1)
-    print("dW2 shape: ", dW2.shape)  # dW2 shape:  (200,)
-    '''
-    # dZ1 = dZ2 multi (W2).T  = np.outer(dZ2, W2), because dZ2 and W2 are both 1 dim vector,
-    # in this case matmul equals to outer
-    dh = np.outer(epdlogp, model['W2'])   # shape: (1227, 200)
-    # dZ1 = dZ1 times g1'(Z1), here we only have A1, but A1 = max(0, Z1), so in the following we check if A1<=0
-    dh[eph <= 0] = 0  # backpro prelu
-    dW1 = np.dot(dh.T, epx)  # dW1 = dZ1.T multi X, dim is (200, 1227) multi (1227, 6400) = (200, 6400)
+def policy_backward(A1, Z1, dZ2, X):
+    m = dZ2.shape[1]
+    W2 = model["W2"]
+
+    dW2 = np.dot(dZ2, A1.T) / m
+    dZ1 = np.dot(W2.T, dZ2)
+    dZ1[Z1 < 0] = 0  # backpro prelu
+    dW1 = np.dot(dZ1, X.T) / m
+
     return {'W1': dW1, 'W2': dW2}
 
 
 env = gym.make("Pong-v0")
 observation = env.reset()
 prev_x = None  # used in computing the difference frame
-xs, hs, dlogps, drs = [], [], [], []
+xs, A1s, Z1s, dZ2s, drs = [], [], [], [], []
 running_reward = None
 reward_sum = 0
 episode_number = 0
@@ -97,18 +94,20 @@ while True:
     cur_x = prepro(observation)
     x = cur_x - prev_x if prev_x is not None else np.zeros(D)
     prev_x = cur_x
+    x = x.reshape((x.shape[0], 1))
 
     # forward the policy network and sample an action from the returned probability
-    aprob, h = policy_forward(x)
-    action = 2 if np.random.uniform() < aprob else 3  # roll the dice! np.random.uniform(): Draw samples from a uniform distribution.
+    A1, A2, Z1 = policy_forward(x)  # only return the result of a single input
+    action = 2 if np.random.uniform() < A2 else 3  # roll the dice! np.random.uniform(): Draw samples from a uniform distribution.
 
     # record various intermediates (needed later for backprop)
     xs.append(x)  # observation
-    hs.append(h)  # hidden state
+    Z1s.append(Z1)  # hidden state
+    A1s.append(A1)  # hidden state
+    # print(len(A1s))
     y = 1 if action == 2 else 0  # a "fake label"
-    # dZ
-    dlogps.append(
-        y - aprob)  # grad that encourages the action that was taken to be taken (see http://cs231n.github.io/neural-networks-2/#losses if confused)
+    # grad that encourages the action that was taken to be taken (see http://cs231n.github.io/neural-networks-2/#losses if confused)
+    dZ2s.append(A2 - y)
 
     # step the environment and get new measurements
     observation, reward, done, info = env.step(action)
@@ -120,11 +119,12 @@ while True:
         episode_number += 1
 
         # stack together all inputs, hidden states, action gradients, and rewards for this episode
-        epx = np.vstack(xs)
-        eph = np.vstack(hs)
-        epdlogp = np.vstack(dlogps)
+        epx = np.hstack(xs)
+        epZ1 = np.hstack(Z1s)
+        epA1 = np.hstack(A1s)
+        epdZ2 = np.hstack(dZ2s)
         epr = np.vstack(drs)
-        xs, hs, dlogps, drs = [], [], [], []  # reset array memory
+        xs, A1s, Z1s, dZ2s, drs = [], [], [], [], []  # reset array memory
 
         # compute the discounted reward backwards through time
         discounted_epr = discount_rewards(epr)
@@ -132,8 +132,8 @@ while True:
         discounted_epr -= np.mean(discounted_epr)
         discounted_epr /= np.std(discounted_epr)
 
-        epdlogp *= discounted_epr  # modulate the gradient with advantage (PG magic happens right here.)
-        grad = policy_backward(eph, epdlogp)
+        epdZ2 *= discounted_epr.T  # modulate the gradient with advantage (PG magic happens right here.)
+        grad = policy_backward(epA1, epZ1, epdZ2, epx)
         for k in model:
             grad_buffer[k] += grad[k]  # accumulate grad over batch
 
@@ -142,14 +142,14 @@ while True:
             for k, v in model.items():
                 g = grad_buffer[k]  # gradient
                 rmsprop_cache[k] = decay_rate * rmsprop_cache[k] + (1 - decay_rate) * g ** 2
-                # here do w = w + a * dw, because the original code is doing gradient ascent
-                model[k] += learning_rate * g / (np.sqrt(rmsprop_cache[k]) + 1e-5)
+                model[k] -= learning_rate * g / (np.sqrt(rmsprop_cache[k]) + 1e-5)  # w = w - a * dw
                 grad_buffer[k] = np.zeros_like(v)  # reset batch gradient buffer
 
         # boring book-keeping
         running_reward = reward_sum if running_reward is None else running_reward * 0.99 + reward_sum * 0.01
         print('resetting env. episode reward total was %f. running mean: %f' % (reward_sum, running_reward))
-        if episode_number % 100 == 0: pickle.dump(model, open('save.p', 'wb'))
+        if episode_number % 100 == 0:
+            pickle.dump(model, open('save.p', 'wb'))
         reward_sum = 0
         observation = env.reset()  # reset env
         prev_x = None

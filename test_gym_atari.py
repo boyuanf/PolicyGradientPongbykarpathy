7c5fa056b84596bc2e96ab2916198bbf86d3d523
boyuanf <fengboyuannj@gmail.com>
2018-05-28 17:29:16 -0400

    be able to debug when loss life

--- test_gym_atari.py
+++ test_gym_atari.py
@@ -3,10 +3,17 @@ import time
 env = gym.make('BreakoutDeterministic-v4')
 env.reset()
 
+start_life = 5
+
 for i in range(10000):
     observe, reward, done, info = env.step(env.action_space.sample())
+    print(reward)
     env.render()
     time.sleep(0.05)
+    if start_life > info['ale.lives']:
+        dead = True
+        start_life = info['ale.lives']
     if done is True:
         print("The episode is done!")
         observe = env.reset()
+        start_life = 5

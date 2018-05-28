df840b61fd8704b699e9c40462299be9ecfe73a9
boyuanf <fengboyuannj@gmail.com>
2018-05-28 16:12:47 -0400

    a better version of test gym

--- test_gym_atari.py
+++ test_gym_atari.py
@@ -1,9 +1,12 @@
 import gym
 import time
-env = gym.make('Pong-v0')
+env = gym.make('BreakoutDeterministic-v4')
 env.reset()
 
-for i in range(1000):
-    env.step(env.action_space.sample())
+for i in range(10000):
+    observe, reward, done, info = env.step(env.action_space.sample())
     env.render()
     time.sleep(0.05)
+    if done is True:
+        print("The episode is done!")
+        observe = env.reset()
